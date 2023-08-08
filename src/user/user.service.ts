5c4f21f7f886d9c34fe3f18b033157294a17557c
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-08-08 11:24:50 +0900

    データ取得

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  findAll() {
-    return 'user servce';
+  constructor(private prismaService: PrismaService) {}
+  async findAll() {
+    const users = await this.prismaService.user.findMany();
+    return users;
   }
 }
